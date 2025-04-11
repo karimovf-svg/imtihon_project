e047ade5cd20751e661e6068fbc5e702a7d85573
karimovf <karimovf_22@icloud.com>
2025-04-11 08:00:24 +0500

    add

---
+++ configapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('post_send_otp/', PhoneSendOTP.as_view()),
+    path('post_v_otp/', VerifySMS.as_view()),
+    path('register/', RegisterUserApi.as_view()),
+    path('token/', LoginApi.as_view(), name='token_obtain_pair'),
+    path('teacher/create/', TeacherCreateApi.as_view(), name='teacher-create'),
+]
